6f3873de39ae0a6bb5b2de4067e7474d32c451df
feofan2004 <varlevlad@gmail.com>
2017-10-16 09:26:45 +0300

    New  5

--- Новый текстовый документ.bat
+++ Новый текстовый документ.bat
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-
-
-REM Here is where you type remark - which will NOT interfere with program / file execution
-
+ECHO OFF
 qmake -o Makefile player_lab.pro
 make
 Start "" "release\player_lab.exe"
 set /P vers= < version.txt
-git add -all
+git add .
 git commit -m "New  %vers%"
 git tag %vers%
 git push -u origin master
-=======
-@ECHO Off
-
-REM Here is where you type remark - which will NOT interfere with program / file execution
-
-Start "" "C:\Users\vlad2_000\Downloads\build-player_lab-Desktop_Qt_5_2_1_MinGW_32bit-Debug\debug\player_lab.exe"
-
-Exit
->>>>>>> 1cc405a32d2e037764fbcd83d36353b2e055d184
+pause
+exit
