d3e0ba31223e1e6bd9bda22500e11724b8904339
feofan2004 <varlevlad@gmail.com>
2017-10-21 22:35:54 +0300

    New  6

---
+++ skript.nsi
@@ -0,0 +1,4 @@
+!define S "Some app"
+!define PN "motivator"
+!define PV "1.0"
+!define dir "C:\Users\vlad2_000\Downloads\PPPO\release"
--- Новый текстовый документ.bat
+++ Новый текстовый документ.bat
@@ -1,11 +1,12 @@
 ECHO OFF
 qmake -o Makefile player_lab.pro
 make
+
 Start "" "release\player_lab.exe"
-set /P vers= < version.txt
+set /P v= < version.txt
 git add .
-git commit -m "New  %vers%"
-git tag %vers%
+git commit -m "New  %v%"
+git tag %v%
 git push -u origin master
 pause
 exit
