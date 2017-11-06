94fdcb4d9ef040f702ea23a662131c0127ff9b4b
feofan2004 <varlevlad@gmail.com>
2017-11-06 09:46:26 +0300

    New  7

--- Новый текстовый документ.bat
+++ Новый текстовый документ.bat
@@ -6,6 +6,10 @@ windeployqt.exe C:\Users\vlad2_000\Downloads\PPPO\release\player_lab.exe
 cd C:\Users\vlad2_000\Downloads\PPPO
 Start "" "release\player_lab.exe"
 set /P v= < version.txt
+set /a v+=1
+copy nul version.txt
+%v%>>version.txt
+compil32 /cc ins.isss
 git add .
 git commit -m "New  %v%"
 git tag %v%
