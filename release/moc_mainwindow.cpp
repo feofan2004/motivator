3650cf07f167687bf9fa19cbdb016b844c6d3631
feofan2004 <varlevlad@gmail.com>
2017-11-20 08:43:50 +0300

    New  11

--- release/moc_mainwindow.cpp
+++ release/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[7];
-    char stringdata[95];
+    QByteArrayData data[8];
+    char stringdata[102];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -30,16 +30,17 @@ struct qt_meta_stringdata_MainWindow_t {
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
 QT_MOC_LITERAL(0, 0, 10),
-QT_MOC_LITERAL(1, 11, 16),
-QT_MOC_LITERAL(2, 28, 0),
-QT_MOC_LITERAL(3, 29, 17),
-QT_MOC_LITERAL(4, 47, 16),
-QT_MOC_LITERAL(5, 64, 16),
-QT_MOC_LITERAL(6, 81, 12)
+QT_MOC_LITERAL(1, 11, 25),
+QT_MOC_LITERAL(2, 37, 0),
+QT_MOC_LITERAL(3, 38, 16),
+QT_MOC_LITERAL(4, 55, 7),
+QT_MOC_LITERAL(5, 63, 7),
+QT_MOC_LITERAL(6, 71, 14),
+QT_MOC_LITERAL(7, 86, 14)
     },
-    "MainWindow\0prevButtonPushed\0\0"
-    "pauseButtonPushed\0playButtonPushed\0"
-    "nextButtonPushed\0updateSlider\0"
+    "MainWindow\0switchCurrentSongTimeMode\0"
+    "\0changeBackground\0openPic\0nextPic\0"
+    "pauseSlideShow\0startSlideShow\0"
 };
 #undef QT_MOC_LITERAL
 
@@ -49,7 +50,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       6,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -57,13 +58,15 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   39,    2, 0x0a,
-       3,    0,   40,    2, 0x0a,
-       4,    0,   41,    2, 0x0a,
-       5,    0,   42,    2, 0x0a,
-       6,    0,   43,    2, 0x0a,
+       1,    0,   44,    2, 0x0a,
+       3,    0,   45,    2, 0x0a,
+       4,    0,   46,    2, 0x0a,
+       5,    0,   47,    2, 0x0a,
+       6,    0,   48,    2, 0x0a,
+       7,    0,   49,    2, 0x0a,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -78,11 +81,12 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
     if (_c == QMetaObject::InvokeMetaMethod) {
         MainWindow *_t = static_cast<MainWindow *>(_o);
         switch (_id) {
-        case 0: _t->prevButtonPushed(); break;
-        case 1: _t->pauseButtonPushed(); break;
-        case 2: _t->playButtonPushed(); break;
-        case 3: _t->nextButtonPushed(); break;
-        case 4: _t->updateSlider(); break;
+        case 0: _t->switchCurrentSongTimeMode(); break;
+        case 1: _t->changeBackground(); break;
+        case 2: _t->openPic(); break;
+        case 3: _t->nextPic(); break;
+        case 4: _t->pauseSlideShow(); break;
+        case 5: _t->startSlideShow(); break;
         default: ;
         }
     }
@@ -114,13 +118,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 6)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 6;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 6)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 6;
     }
     return _id;
 }
--- release/moc_player.cpp
+++ release/moc_player.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_Player_t {
-    QByteArrayData data[1];
-    char stringdata[8];
+    QByteArrayData data[15];
+    char stringdata[235];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -29,9 +29,29 @@ struct qt_meta_stringdata_Player_t {
     )
 static const qt_meta_stringdata_Player_t qt_meta_stringdata_Player = {
     {
-QT_MOC_LITERAL(0, 0, 6)
+QT_MOC_LITERAL(0, 0, 6),
+QT_MOC_LITERAL(1, 7, 27),
+QT_MOC_LITERAL(2, 35, 0),
+QT_MOC_LITERAL(3, 36, 1),
+QT_MOC_LITERAL(4, 38, 30),
+QT_MOC_LITERAL(5, 69, 25),
+QT_MOC_LITERAL(6, 95, 25),
+QT_MOC_LITERAL(7, 121, 19),
+QT_MOC_LITERAL(8, 141, 7),
+QT_MOC_LITERAL(9, 149, 18),
+QT_MOC_LITERAL(10, 168, 11),
+QT_MOC_LITERAL(11, 180, 12),
+QT_MOC_LITERAL(12, 193, 14),
+QT_MOC_LITERAL(13, 208, 12),
+QT_MOC_LITERAL(14, 221, 12)
     },
-    "Player\0"
+    "Player\0setCurrentSongDurationLabel\0\0"
+    "a\0setCurrentSongCurrentTimeLabel\0"
+    "nextSongInPlaylistManager\0"
+    "prevSongInPlaylistManager\0openPlaylistsWidget\0"
+    "addSong\0addFuckingPlaylist\0delPlaylist\0"
+    "playThisSong\0deleteThisSong\0savePlaylist\0"
+    "loadPlaylist\0"
 };
 #undef QT_MOC_LITERAL
 
@@ -41,22 +61,64 @@ static const uint qt_meta_data_Player[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+      12,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   74,    2, 0x0a,
+       4,    1,   77,    2, 0x0a,
+       5,    0,   80,    2, 0x0a,
+       6,    0,   81,    2, 0x0a,
+       7,    0,   82,    2, 0x0a,
+       8,    0,   83,    2, 0x0a,
+       9,    0,   84,    2, 0x0a,
+      10,    0,   85,    2, 0x0a,
+      11,    1,   86,    2, 0x0a,
+      12,    0,   89,    2, 0x0a,
+      13,    0,   90,    2, 0x0a,
+      14,    0,   91,    2, 0x0a,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::LongLong,    3,
+    QMetaType::Void, QMetaType::LongLong,    3,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QModelIndex,    3,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void Player::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Player *_t = static_cast<Player *>(_o);
+        switch (_id) {
+        case 0: _t->setCurrentSongDurationLabel((*reinterpret_cast< qint64(*)>(_a[1]))); break;
+        case 1: _t->setCurrentSongCurrentTimeLabel((*reinterpret_cast< qint64(*)>(_a[1]))); break;
+        case 2: _t->nextSongInPlaylistManager(); break;
+        case 3: _t->prevSongInPlaylistManager(); break;
+        case 4: _t->openPlaylistsWidget(); break;
+        case 5: _t->addSong(); break;
+        case 6: _t->addFuckingPlaylist(); break;
+        case 7: _t->delPlaylist(); break;
+        case 8: _t->playThisSong((*reinterpret_cast< QModelIndex(*)>(_a[1]))); break;
+        case 9: _t->deleteThisSong(); break;
+        case 10: _t->savePlaylist(); break;
+        case 11: _t->loadPlaylist(); break;
+        default: ;
+        }
+    }
 }
 
 const QMetaObject Player::staticMetaObject = {
@@ -83,6 +145,15 @@ int Player::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     _id = QObject::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 12)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 12;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 12)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 12;
+    }
     return _id;
 }
 QT_END_MOC_NAMESPACE
